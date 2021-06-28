4f02075b30e49d14610f96c524d592a61c9792fc
vvoicehovics <vladislav.voicehovich@gmail.com>
2021-06-28 18:35:08 +0300

    add xcframeworks instead of fat libs

---
+++ SDK.xcframework/ios-arm64_i386_x86_64-simulator/KumulosSDK.framework/Headers/KSAPIResponse.h
@@ -0,0 +1,24 @@
+//
+//  KSResponse.h
+//  KumulosSDK
+//
+//  Copyright © 2016 kumulos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ * Holds the results of an RPC API method call
+ */
+@interface KSAPIResponse : NSObject
+
+/// The result data of the Kumulos API method
+@property (nonatomic,readonly) id payload;
+@property (nonatomic,readonly) NSNumber* requestProcessingTime;
+@property (nonatomic,readonly) NSNumber* requestReceivedTime;
+@property (nonatomic,readonly) NSNumber* responseCode;
+@property (nonatomic,readonly) NSString* responseMessage;
+@property (nonatomic,readonly) NSNumber* timestamp;
+@property (nonatomic,readonly) NSNumber* maxProcessingTime;
+
+@end
---
+++ SDKExtension.xcframework/ios-arm64_armv7/KumulosSDKExtension.framework/Headers/KumulosSDKExtension.h
@@ -0,0 +1,18 @@
+//
+//  KumulosSDKExtension.h
+//  KumulosSDKExtension
+//
+//  Created by Vladislav Voicehovics on 05/12/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KumulosSDKExtension.
+FOUNDATION_EXPORT double KumulosSDKExtensionVersionNumber;
+
+//! Project version string for KumulosSDKExtension.
+FOUNDATION_EXPORT const unsigned char KumulosSDKExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KumulosSDKExtension/PublicHeader.h>
+
+#import "KumulosNotificationService.h"
